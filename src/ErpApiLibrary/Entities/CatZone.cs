14de9a6b0502901fa782c920235c1f9dd2d5c0ff
Jack Tsai <wa0327@gmail.com>
2013-05-06 13:59:19 +0800

    add CatZone.cs and CategoryModel.cs

---
+++ src/ErpApiLibrary/Entities/CatZone.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Monday.DataAccess.Common;
+
+namespace ErpApi.Entities
+{
+    /// <summary>
+    /// mapping table name: catzone。
+    /// </summary>
+    public class CatZone
+    {
+        /// <summary>
+        /// catzone_id。
+        /// </summary>
+        [DBColumnMapping("catzone_id")]
+        public short Id { get; set; }
+
+        /// <summary>
+        /// catzone_name。
+        /// </summary>
+        [DBColumnMapping("catzone_name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// catzone_sort。
+        /// </summary>
+        [DBColumnMapping("catzone_sort")]
+        public short Sort { get; set; }
+
+        /// <summary>
+        /// 隸屬PM部門。
+        /// </summary>
+        [DBColumnMapping("catzone_pmdep")]
+        public string PmDep { get; set; }
+
+        /// <summary>
+        /// catzone_user。
+        /// </summary>
+        [DBColumnMapping("catzone_user")]
+        public string User { get; set; }
+
+        /// <summary>
+        /// catzone_sysdate。
+        /// </summary>
+        [DBColumnMapping("catzone_sysdate")]
+        public DateTime SysDate { get; set; }
+
+        /// <summary>
+        /// catzone_updated。
+        /// </summary>
+        [DBColumnMapping("catzone_updated")]
+        public byte Updated { get; set; }
+
+        /// <summary>
+        /// catzone_updateddate。
+        /// </summary>
+        [DBColumnMapping("catzone_updateddate")]
+        public DateTime UpdatedDate { get; set; }
+
+        /// <summary>
+        /// catzone_updateduser。
+        /// </summary>
+        [DBColumnMapping("catzone_updateduser")]
+        public string UpdatedUser { get; set; }
+
+        /// <summary>
+        /// 區類別。
+        /// </summary>
+        [DBColumnMapping("catzone_type")]
+        public byte TypeId { get; set; }
+
+        /// <summary>
+        /// 區類別名稱。
+        /// </summary>
+        [DBColumnMapping("catzone_typename")]
+        public string TypeName { get; set; }
+    }
+}
---
+++ src/ErpApiLibrary/Entities/CategoryModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ErpApi.Entities
+{
+    /// <summary>
+    /// 子站相關資訊
+    /// </summary>
+    public sealed class CategoryModel
+    {
+        /// <summary>
+        /// CatSub
+        /// </summary>
+        public CatSub CatSub { get; set; }
+
+        /// <summary>
+        /// Catzone
+        /// </summary>
+        public CatZone CatZone { get; set; }
+    }
+}
